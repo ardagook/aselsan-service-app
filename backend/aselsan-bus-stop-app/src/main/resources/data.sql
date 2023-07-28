fa38d57a9b2df136dd1daf6b2fd75079aae46ba8
ardagook <arda.gok@tedu.edu.tr>
2023-07-28 15:28:44 +0300

    update data changes

--- backend/aselsan-bus-stop-app/src/main/resources/data.sql
+++ backend/aselsan-bus-stop-app/src/main/resources/data.sql
@@ -1,23 +1,16 @@
 INSERT INTO bus_entity (id,no,name) VALUES
-                                   (nextval('bus_sequence'),'B9','Aselsan Gölbaşı Tesisleri ⇄ Basınevleri-İncirli');
+                                   (nextval('bus_sequence'),'C15','Aselsan Gölbaşı Tesisleri ⇄ Kolej-Kurtuluş');
 
 
 INSERT INTO stop_entity (id, no, stop_name, geometry) VALUES
-                                                          (nextval('stop_sequence'), '5000', 'Aselsan Gölbaşı Tesisleri Otobüs Toplanma Alanı', 'POINT(32.81527595300261 39.71809809301061)'),
-                                                          (nextval('stop_sequence'), '5001', 'Akköprü ', 'POINT(32.83507325561155 39.95120645256165)'),
-                                                          (nextval('stop_sequence'), '5002', 'Varlık Toki', 'POINT(32.83801006877974 39.95716707892828)'),
-                                                          (nextval('stop_sequence'), '5003', 'Gülhane Ortaokulu', 'POINT(32.85362081397503 39.96857263342855)'),
-                                                          (nextval('stop_sequence'), '5004', 'İncirli', 'POINT(32.84743978676053 39.978469288331866)'),
-                                                          (nextval('stop_sequence'), '5005', 'Faik Erbağı', 'POINT(32.85043739035399 39.981350499912196)'),
-                                                          (nextval('stop_sequence'), '5006', 'Karaelmas Durağı', 'POINT(32.849659772465344 39.98338951907692)'),
-                                                          (nextval('stop_sequence'), '5007', 'Danışment Taksi', 'POINT(32.853007282646075 39.984648136608286)');
-
-INSERT INTO bus_stops (bus_id, stop_id)  VALUES
-                                                                                                     (106,1007),
-                                                                                                     (106,1008),
-                                                                                                     (106,1009),
-                                                                                                     (106,1010),
-                                                                                                     (106,1011),
-                                                                                                     (106,1012),
-                                                                                                     (106,1013),
-                                                                                                     (106,1014);
+                                                          (nextval('stop_sequence'), '5006', 'Tren Garı', 'POINT(32.842686377482664 39.93407216301466)'),
+                                                          (nextval('stop_sequence'), '5007', 'Sıhıyye Köprüsü', 'POINT(32.854391525964814 39.92881161185775)'),
+                                                          (nextval('stop_sequence'), '5008', 'Sıhhıye Alt dörtyol', 'POINT(32.85821716891152 39.92863437723278)'),
+                                                          (nextval('stop_sequence'), '5009', 'Kurtuluş Parkı', 'POINT(32.86657491468003 39.92822935865337)'),
+                                                          (nextval('stop_sequence'), '5010', 'Kurtuluş Parkı2', 'POINT(32.86809560048184 39.92802718250931)');
+INSERT INTO bus_stops (bus_id, stop_id)  VALUES                                                      (107,1007),
+                                                                                                     (107,1015),
+                                                                                                     (107,1016),
+                                                                                                     (107,1017),
+                                                                                                     (107,1018),
+                                                                                                     (107,1019);
