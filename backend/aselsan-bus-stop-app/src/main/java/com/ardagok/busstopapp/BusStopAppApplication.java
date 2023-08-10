e76c099cd8ba01e097f5532e20eda9f5ed40a06b
ardagook <arda.gok@tedu.edu.tr>
2023-08-10 14:31:32 +0300

    swaggerui

--- backend/aselsan-bus-stop-app/src/main/java/com/ardagok/busstopapp/BusStopAppApplication.java
+++ backend/aselsan-bus-stop-app/src/main/java/com/ardagok/busstopapp/BusStopAppApplication.java
@@ -1,9 +1,28 @@
 package com.ardagok.busstopapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Aselsan-Service-App",
+                version = "1.0.0",
+                description = "Aselsan Intern Project Arda Gök",
+                contact = @Contact(
+                        name = "Arda Gök",
+                        email = "ardagook7@gmail.com"
+                ),
+                license = @License(
+                        name = "License",
+                        url = "ardagook"
+                )
+        )
+)
 public class BusStopAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusStopAppApplication.class, args);
